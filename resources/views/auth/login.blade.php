261e10a9818ba0de82ed00f50525ea9b709cedbd
Sayed Abu Zayed Hossain <zayedbd24@gmail.com>
2022-10-16 14:04:53 +0600

    login and registration form design modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,64 +1,93 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            {{-- <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a> --}}
-            <div>
-                <img src="{{url('assets/img/phonebook.jpg')}}" class="image text-center" alt="image" width="120px">
-            </div>
-        </x-slot>
-       
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<!doctype html>
+<html lang="en">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<head>
+	<title>{{ config('app.name', 'Laravel') }} | Login</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+	<link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{url('assets/css/logregstyle.css')}}">
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+</head>
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+<body>
+	<section class="ftco-section">
+		<div class="container">
+			<!-- <div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Login #05</h2>
+				</div>
+			</div> -->
+			<div class="row justify-content-center">
+				<div class="col-md-7 col-lg-5">
+					<div class="wrap">
+						<!-- <div class="img" style="background-image: url(images/bg-1.jpg);"></div> -->
+						<div class="login-wrap p-4 p-md-5">
+							<div class="d-flex">
+								<div class="w-100">
+									<h3 class="mb-4">Login</h3>
+								</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="#"
+											class="social-icon d-flex align-items-center justify-content-center"><span
+												class="fa fa-facebook"></span></a>
+										<a href="#"
+											class="social-icon d-flex align-items-center justify-content-center"><span
+												class="fa fa-twitter"></span></a>
+									</p>
+								</div>
+							</div>
+							<form action="{{ route('login') }}" method="POST" class="signin-form">
+                                @csrf
+								<div class="form-group mt-3">
+									<input type="email" class="form-control" name="email" required>
+									<label class="form-control-placeholder" for="username">Email</label>
+								</div>
+								<div class="form-group">
+									<input id="password-field" type="password" name="password" class="form-control" required>
+									<label class="form-control-placeholder" for="password">Password</label>
+									<span toggle="#password-field"
+										class="fa fa-fw fa-eye field-icon toggle-password"></span>
+								</div>
+								<div class="form-group">
+									<button type="submit" class="form-control btn btn-primary rounded submit px-3">Login</button>
+								</div>
+								<div class="form-group d-md-flex">
+									<div class="w-50 text-left">
+										<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+											<input type="checkbox">
+											<span class="checkmark"></span>
+										</label>
+									</div>
+                                    @if (Route::has('password.request'))
+									<div class="w-50 text-md-right">
+										<a href="{{ route('password.request') }}">Forgot Password</a>
+									</div>
+                                    @endif
+								</div>
+							</form>
+							<p class="text-center">Not a member? <a data-toggle="tab" href="{{ route('register') }}">Register</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+	<script src="{{url('assets/js/jquery-3.6.0.min.js')}}"></script>
+	<script src="{{url('assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{url('assets/js/logreg.js')}}"></script>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+	{{-- <script src="js/jquery.min.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/main.js"></script> --}}
 
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
-            </div>
-            <div class="flex items-center mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ __('Create an Account?') }}
-                </a>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,80 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <div>
-                <img src="{{url('assets/img/phonebook.jpg')}}" class="image text-center" alt="image" width="120px">
-            </div>
-        </x-slot>
+<!doctype html>
+<html lang="en">
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<head>
+	<title>{{ config('app.name', 'Laravel') }} | Register</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+	<link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{url('assets/css/logregstyle.css')}}">
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
+</head>
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-7 col-lg-5">
+					<div class="wrap">
+						<div class="login-wrap p-4 p-md-5">
+							<div class="d-flex">
+								<div class="w-100">
+									<h3 class="mb-4">Register</h3>
+								</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="#"
+											class="social-icon d-flex align-items-center justify-content-center"><span
+												class="fa fa-facebook"></span></a>
+										<a href="#"
+											class="social-icon d-flex align-items-center justify-content-center"><span
+												class="fa fa-twitter"></span></a>
+									</p>
+								</div>
+							</div>
+							<form method="POST" action="{{ route('register') }}" class="signin-form">
+                                @csrf
+								<div class="form-group mt-3">
+									<input type="text" name="name" class="form-control" required>
+									<label class="form-control-placeholder" for="name">Name</label>
+								</div>
+								<div class="form-group mt-3">
+									<input type="email" name="email" class="form-control" required>
+									<label class="form-control-placeholder" for="email">Email</label>
+								</div>
+								<div class="form-group">
+									<input id="password-field" type="password" name="password" class="form-control" required>
+									<label class="form-control-placeholder" for="password">Password</label>
+									<span toggle="#password-field"
+										class="fa fa-fw fa-eye field-icon toggle-password"></span>
+								</div>
+								<div class="form-group">
+									<input id="password_confirmation" type="password" name="password_confirmation" class="form-control" required>
+									<label class="form-control-placeholder" for="cpassword">Confirm Password</label>
+									<span toggle="#password_confirmation"
+										class="fa fa-fw fa-eye field-icon toggle-password"></span>
+								</div>
+								<div class="form-group">
+									<button type="submit" class="form-control btn btn-primary rounded submit px-3">Register</button>
+								</div>
+							</form>
+							<p class="text-center">Already Registred? <a data-toggle="tab" href="{{ route('login') }}">Login</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+	<script src="{{url('assets/js/jquery-3.6.0.min.js')}}"></script>
+	<script src="{{url('assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{url('assets/js/logreg.js')}}"></script>
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
+</body>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
